50eafcdc08d969c19135048d299dd5fdcda3f777
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-07-23 20:36:14 +0300

    ...

--- skoda_login.sh
+++ skoda_login.sh
@@ -4,11 +4,13 @@ set -eu
 
 # Usage: SKODA_USER=foo@example.com SKODA_PASSWORD=mypass ./skoda_login.sh <1|2>
 
-if [ "$1" == "1" ]; then
+endpoint=$1
+
+if [ "$endpoint" == "1" ]; then
     CLIENT_ID="d2d5cfa4-fdeb-44cb-a621-853c649748ad%40apps_vw-dilab_com"
     REDIRECT="https%3A%2F%2Fwww.skoda-connect.com%2Fwrapper-callback"
     SCOPE="openid%20mbb"
-elif [ "$1" == "2" ]; then
+elif [ "$endpoint" == "2" ]; then
     CLIENT_ID="7f045eee-7003-4379-9968-9355ed2adb06@apps_vw-dilab_com"
     REDIRECT="skodaconnect%3A%2F%2Foidc.login%2F"
     SCOPE="openid%20profile%20address%20cars%20email%20birthdate%20badge%20mbb%20phone%20driversLicense%20dealers%20profession%20vin%20mileage"
@@ -31,16 +33,16 @@ login() {
 
     params2=$(identifier | jq -r '.hmac')
 
-    curl -Ls -b /tmp/skoda-cookies.txt -i -D - -o /dev/null "https://identity.vwgroup.io/signin-service/v1/${CLIENT_ID}/login/authenticate" -d "hmac=${params2}&${paramsOther}email=${SKODA_USER}&password=$SKODA_PASSWORD" | grep 'location:' | tail -n1 | sed 's/.*code=\(.*\)/\1/' > /tmp/skoda-token
+    curl -Ls -b /tmp/skoda-cookies.txt -i -D - -o /dev/null "https://identity.vwgroup.io/signin-service/v1/${CLIENT_ID}/login/authenticate" -d "hmac=${params2}&${paramsOther}email=${SKODA_USER}&password=$SKODA_PASSWORD" | grep 'location:' | tail -n1 | sed 's/.*code=\(.*\)/\1/' > "/tmp/skoda-token-$endpoint"
 
     rm /tmp/skoda-cookies.txt
 }
 
-if [ ! -f "/tmp/skoda-token" ]; then
+if [ ! -f "/tmp/skoda-token-$endpoint" ]; then
     login
 fi
-for i in $(find /tmp/skoda-token -mmin +5); do
+for i in $(find /tmp/skoda-token-"$endpoint" -mmin +5); do
     login
 done
 
-cat /tmp/skoda-token
+cat /tmp/skoda-token-"$endpoint"

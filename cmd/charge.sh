480919e14c5c77adc679fc08d19ead03f43fcf47
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-07-29 12:52:36 +0300

    nix deps

--- cmd/charge.sh
+++ cmd/charge.sh
@@ -20,7 +20,7 @@ else
     ret=$(curl -s -H "Authorization: Bearer $token" https://api.connect.skoda-auto.cz/api/v1/charging/${SKODA_VIN}/status | jq -r '.charging.state')
     if [ "$ret" == "Charging" ]; then
         echo 1
-    elif [ "$ret" == "ReadyForCharging" ]; then
+    elif [ "$ret" == "ReadyForCharging" ] || [ "$ret" == "NotReadyForCharging" ]; then
         echo 0
     else
         echo "$ret"

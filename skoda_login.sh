44e41ac9665dac32e9cb9ab9fe1b6e42c1d2b052
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-07-29 13:06:12 +0300

    nix deps

--- skoda_login.sh
+++ skoda_login.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep SKODA_USER --keep SKODA_PASSWORD -i bash -I channel:nixos-23.05-small -p bash cacert curl jq yq htmlq flock gnugrep gnused
+#! nix-shell --pure --keep SKODA_USER --keep SKODA_PASSWORD -i bash -I channel:nixos-23.05-small -p bash cacert curl jq yq htmlq flock gnugrep gnused findutils
 set -eu
 
 # Usage: SKODA_USER=foo@example.com SKODA_PASSWORD=mypass ./skoda_login.sh <1|2>

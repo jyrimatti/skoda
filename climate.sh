cea9d9928cb1f0a55e4dc0325973d35220c1cf4d
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-08-27 23:29:43 +0300

    ...

--- climate.sh
+++ climate.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p cacert curl jq nix
+#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p cacert curl jq nix dash
 set -eu
 
 # Usage: ./charging.sh
--- cmd/batteryPercentage.sh
+++ cmd/batteryPercentage.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p jq nix
+#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p jq nix dash
 set -eu
 
 # Usage: ./batteryPercentage.sh

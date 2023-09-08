8584593b6b61799ef4c4f88bbc52d83add2fb2b7
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-09-08 22:05:03 +0300

    ...

--- cmd/hoursNeededToCharge.sh
+++ cmd/hoursNeededToCharge.sh
@@ -20,4 +20,4 @@ CAPACITY_KWH=$1
 
 batteryPercentage=$(./cmd/batteryPercentage.sh)
 
-echo "(100 - $batteryPercentage)/100 * $CAPACITY_KWH / $POWER_KW" | bc -l
+printf %.1f "$("(100 - $batteryPercentage)/100 * $CAPACITY_KWH / $POWER_KW" | bc -l)"

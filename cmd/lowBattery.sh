e26c5adc9d67ff83d33e74dcef03cc4339da932c
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-01-15 22:11:29 +0200

    update nix

--- cmd/lowBattery.sh
+++ cmd/lowBattery.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p jq nix dash
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p jq nix dash
 set -eu
 
 # Usage: ./lowBattery.sh
--- skoda_login.sh
+++ skoda_login.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep SKODA_USER --keep SKODA_PASSWORD -i dash -I channel:nixos-23.05-small -p dash cacert curl jq yq htmlq flock gnugrep gnused findutils
+#! nix-shell --pure --keep SKODA_USER --keep SKODA_PASSWORD -i dash -I channel:nixos-23.11-small -p dash cacert curl jq yq htmlq flock gnugrep gnused findutils
 set -eu
 
 # Usage: SKODA_USER=foo@example.com SKODA_PASSWORD=mypass ./skoda_login.sh <1|2>

7f439d5b81b065e197c4e9e75b64d722a1e08e29
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-14 19:03:16 +0300

    CREDENTIALS_DIRECTORY

--- rvs.sh
+++ rvs.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p cacert curl jq nix dash
+#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p cacert curl jq nix dash
 set -eu
 
 # Usage: ./charging.sh
--- skoda_env.sh
+++ skoda_env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export SKODA_USER="$(cat .skoda-user)"
-export SKODA_PASSWORD="$(cat .skoda-pass)"
-export SKODA_VIN="$(cat .skoda-vin)"
+export SKODA_USER="$(cat "${CREDENTIALS_DIRECTORY:-.}/.skoda-user")"
+export SKODA_PASSWORD="$(cat "${CREDENTIALS_DIRECTORY:-.}/.skoda-pass")"
+export SKODA_VIN="$(cat "${CREDENTIALS_DIRECTORY:-.}/.skoda-vin")"

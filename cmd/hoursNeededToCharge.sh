15ee9ac91791a17f9b602bfaca768290f58b1c0d
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-01-21 20:37:58 +0200

    update nix

--- cmd/hoursNeededToCharge.sh
+++ cmd/hoursNeededToCharge.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p bc nix dash getoptions
+#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-24.11-small -p bc nix dash getoptions
 set -eu
 
 POWER_KW=2.3
--- cmd/secondsRemainingToHeat.sh
+++ cmd/secondsRemainingToHeat.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p jq nix bc dash
+#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-24.11-small -p jq nix bc dash
 set -eu
 
 # Usage: ./secondsRemainingToHeat.sh
